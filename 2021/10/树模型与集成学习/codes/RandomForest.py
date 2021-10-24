58d3524d3c647284d12b96fcd520f93204fcd1be
cHEn <76734087+chenjiyan2001@users.noreply.github.com>
2021-10-25 01:27:22 +0800

    Add files via upload

--- 2021/10/树模型与集成学习/codes/RandomForest.py
+++ 2021/10/树模型与集成学习/codes/RandomForest.py
@@ -1,1 +1,60 @@
+import numpy as np
+from sklearn.metrics import accuracy_score, r2_score
+from sklearn.datasets import make_classification
+from sklearn.ensemble import RandomForestClassifier as RF
+import sys
+sys.path.append(".")
+from CART import DecisionTreeClassifier, DecisionTreeRegressor
 
+class RandomForestClassifier:
+    def __init__(self, n_estimators, max_depth):
+        self.n_estimators = n_estimators
+        self.max_depth = max_depth
+        self.trees = []
+        self.oobs = []
+        self.weights = []
+    
+    def _get_oob_score(self, X, y):
+        for tree, oob_idx in zip(self.trees, self.oobs):
+            y_pred = tree.predict(X[oob_idx])
+            acc = accuracy_score(y[oob_idx], y_pred)
+            self.weights.append(acc)
+
+    def fit(self, X, y):
+        for tree_id in range(self.n_estimators):
+            index = np.random.randint(0, X.shape[0], X.shape[0])
+            oob_idx = list(set(range(X.shape[0])) - set(index))
+            random_X = X[index]
+            random_y = y[index]
+            tree = DecisionTreeClassifier(self.max_depth)
+            tree.fit(random_X, random_y)
+            self.trees.append(tree)
+            self.oobs.append(oob_idx)
+        self._get_oob_score(X, y)
+    
+
+    def predict(self, X):
+        results = []
+
+        for x in X:
+            estimator_result = []
+            for tree in self.trees:
+                estimator_result.append(tree.predict(x.reshape(1, -1))[0])
+
+            results.append(np.argmax([weight*result for result, weight in zip(estimator_result, self.weights)]))  # 返回该样本的预测结果，采取方案：多数投票
+        return np.array(results)
+
+
+if __name__ == "__main__":
+    X, y = make_classification(n_samples=200, n_features=8, n_informative=4, random_state=2)
+
+    RF1 = RandomForestClassifier(n_estimators=10, max_depth=3)
+    RF2 = RF(n_estimators=10, max_depth=3)
+
+    RF1.fit(X, y)
+    res1 = RF1.predict(X)
+
+    RF2.fit(X, y)
+    res2 = RF2.predict(X)
+
+    print('结果一样的比例', (np.abs(res1 - res2) < 1e-5).mean())
